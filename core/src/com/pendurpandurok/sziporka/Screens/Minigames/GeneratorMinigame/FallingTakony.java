41e25f987b0d3bda16d12d7fc2950be2cad7edb6
Martin Horváth <pigfighter2@gmail.com>
2019-12-05 21:50:24 +0100

    Itt van Márton

--- core/src/com/pendurpandurok/sziporka/Screens/Minigames/GeneratorMinigame/FallingTakony.java
+++ core/src/com/pendurpandurok/sziporka/Screens/Minigames/GeneratorMinigame/FallingTakony.java
@@ -1,14 +1,21 @@
 package com.pendurpandurok.sziporka.Screens.Minigames.GeneratorMinigame;
 
+import com.pendurpandurok.sziporka.Assets;
 import com.pendurpandurok.sziporka.Screens.Minigames.ShootUpMinigame.Takony;
 
-public class FallingTakony extends Takony {
+import hu.csanyzeg.master.MyBaseClasses.Scene2D.OneSpriteStaticActor;
+
+public class FallingTakony extends OneSpriteStaticActor {
 
     float fallingSpeed;
 
     public FallingTakony(float fallingSpeed) {
-        super();
+        super(Assets.manager.get(Assets.TAKONY));
         this.fallingSpeed = fallingSpeed;
+
+        setSize(getWidth() / 10, getHeight() / 10);
+        addBaseCollisionCircleShape();
+
     }
 
     @Override
